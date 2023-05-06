d06499c369ccdb3f4f220d2f64f8a6924f682fe5
AdsonBruno <adsonbruno2@gmail.com>
2023-05-06 15:29:47 -0300

    feat: add OrderRepository and add method

---
+++ src/repository/OrderRepository.js
@@ -0,0 +1,26 @@
+const orderTeste = require('../model/order');
+const path = require('path');
+
+class OrderRepositroy {
+  constructor() {
+    this.orders = [];
+  }
+
+  add(order) {
+    console.log(`validate: ${order.validate()}`)
+    if (order instanceof orderTeste && order.validate()) {
+      this.orders.push(order);
+      console.log('Pedido adicionado com sucesso!');
+    } else {
+      console.log('erro');
+      throw new Error(`Invalid order: ${JSON.stringify(order)}`);
+    }
+    console.log(order)
+    return order;
+  }
+
+  get() {
+    return this.orders;
+  }
+
+}
