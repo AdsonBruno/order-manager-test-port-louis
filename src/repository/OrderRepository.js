70652ec3cd6effd355e12b28ff86bd8944627cff
AdsonBruno <adsonbruno2@gmail.com>
2023-05-07 21:59:13 -0300

    feat: add readOrders

--- src/repository/OrderRepository.js
+++ src/repository/OrderRepository.js
@@ -1,10 +1,40 @@
 const orderTeste = require('../model/order');
 const path = require('path');
+const fs = require('fs');
+const OrderTeste = require('../model/order');
 
 class OrderRepository {
   constructor() {
     this.orders = [];
   }
+
+  async readOrders(directoryPath) {
+    const files = await fs.promises.readdir(directoryPath);
+    const orderFiles = files.filter((file) => path.extname(file) === '.txt');
+
+    for (const file of orderFiles) {
+      const filePath = path.join(directoryPath, file);
+      const content = await fs.promises.readFile(filePath, 'utf-8');
+      const orderData = content
+        .trim()
+        .split('\n')
+        .map((line) => JSON.parse(line));
+
+      const orders = orderData.map((orderData) => {
+        const order = new OrderTeste(
+          orderData.número_item,
+          orderData.código_produto,
+          orderData.quantidade_produto,
+          orderData.valor_unitário_produto
+        );
+        return order;
+      }
+      );
+      console.log(orders);
+      this.orders = this.orders.concat(orders);
+    }
+  }
+
 
   add(order) {
     console.log(`validate: ${order.validate()}`)
@@ -25,4 +55,4 @@ class OrderRepository {
 
 }
 
-module.exports = OrderRepository;
+module.exports = new OrderRepository();
