0c929335bad1c9062ccf8b5372a3cf5d8ebac5ef
AdsonBruno <adsonbruno2@gmail.com>
2023-05-11 15:55:10 -0300

    feat: add orderItem.dto.js

---
+++ src/model/orderItem.dto.js
@@ -0,0 +1,9 @@
+export default class OrderItem {
+  constructor(número_item, código_produto, quantidade_produto, valor_unitário_produto) {
+
+    this.número_item = número_item;
+    this.código_produto = código_produto;
+    this.quantidade_produto = quantidade_produto;
+    this.valor_unitário_produto = valor_unitário_produto;
+  }
+}
