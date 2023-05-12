1297fffcfeca14598a4b5c07bb59c47f7f7cb9d5
AdsonBruno <adsonbruno2@gmail.com>
2023-05-12 11:18:06 -0300

    refactor: delete model folder

--- src/model/invoice.js
+++
@@ -1,9 +0,0 @@
-class Invoice {
-  constructor(order_id, item_number, product_quantity) {
-    this.order_id = order_id;
-    this.item_number = item_number;
-    this.product_quantity = product_quantity;
-  }
-}
-
-module.exports = Invoice;
--- src/model/orderItem.dto.js
+++
@@ -1,9 +0,0 @@
-export default class OrderItem {
-  constructor(número_item, código_produto, quantidade_produto, valor_unitário_produto) {
-
-    this.número_item = número_item;
-    this.código_produto = código_produto;
-    this.quantidade_produto = quantidade_produto;
-    this.valor_unitário_produto = valor_unitário_produto;
-  }
-}
